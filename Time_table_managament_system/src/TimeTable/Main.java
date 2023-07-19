6bd573034af461287bfc283d5ecd734df3f82f75
GowriManohar-coder <129403677+GowriManohar-coder@users.noreply.github.com>
2023-07-19 18:34:14 +0530

    Add files via upload

---
+++ Time_table_managament_system/src/TimeTable/Main.java
@@ -0,0 +1,17 @@
+package TimeTable;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("----Time Table-----");
+		TimeTable tt = new TimeTable();
+		tt.dataentry();
+
+	}
+
+}
